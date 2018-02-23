4ea80ba701fe0548956f7a1c653eb76e6ecb45bb
June Tate-Gans <jtgans@google.com>
2018-02-23 15:58:22 -0800

    permissions: Add a script to correct them post-overlay
    This adds a script and data file to fixup permissions that are altered
    during the overlay rsync process. Unfortunately, we can't store these
    permissions in the git repo, so this script is required.
    
    Change-Id: I3f0a09c9395f858c20ae3d323bb7d698db90a25b

---
+++ tools/fix_permissions.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+    echo "Usage: fix_permissions.sh -p <permissions_file> -t <rootfs_dir>"
+    exit 1
+}
+
+apply_permissions() {
+    local dentry
+    local flags
+    local flag
+    local opts
+    local user
+    local group
+    local file_mode
+
+    cd $ROOTFS
+
+    while read dentry flags user group file_mode; do
+        if [[ $dentry =~ ^# ]] || [[ -z $dentry ]]; then
+            continue
+        fi
+
+        opts=""
+
+        set -- $(echo $flags |sed 's/,/ /g') --
+        for flag; do
+            case "$flag" in
+                recurse)
+                    opts="$opts -R"
+                    shift
+                    ;;
+                -)
+                    opts=""
+                    shift
+                    ;;
+                --)
+                    shift
+                    break
+                    ;;
+            esac
+        done
+
+        echo chmod $opts $file_mode $dentry
+        echo chown $opts $user:$group $dentry
+        chmod $opts $file_mode $dentry
+        chown $opts $user:$group $dentry
+    done
+}
+
+PERMISSIONS=""
+ROOTFS=""
+ARGS=$(getopt hp:t: $*)
+set -- $ARGS
+
+for i; do
+    case "$1" in
+        -p)  # permissions_file
+            PERMISSIONS="$2"
+            shift 2
+            ;;
+        -t)  # rootfs_dir
+            ROOTFS="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -h|*)
+            usage
+            ;;
+    esac
+done
+
+if [[ -z $PERMISSIONS ]] || [[ -z $ROOTFS ]]; then
+    usage
+fi
+
+apply_permissions < $PERMISSIONS
