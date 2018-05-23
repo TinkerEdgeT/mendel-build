53966abc097512b90cf601a9659dbeff9b3157d2
June Tate-Gans <jtgans@google.com>
2018-05-23 13:59:14 -0700

    template: Fix variable names
    When I wrote this originally, I kept thinking target, target, target for
    some reason. This fixes this unfortunate oversight.
    
    Change-Id: If22c24ef70147dee27fc1fa4b011eaa55e40034f

--- template.mk
+++ template.mk
@@ -8,14 +8,14 @@ include $(ROOTDIR)/build/preamble.mk
 # Define your top-level build targets here.
 ####
 
-template: $(TARGET_OUT)/template
+template: $(PRODUCT_OUT)/template
 
 ####
 # Other rules you need to define to do the build go here.
 ####
 
 clean::
-	rm -f $(TARGET_OUT)/template
+	rm -f $(PRODUCT_OUT)/template
 
 targets::
 	echo "template - replace this with better information"
