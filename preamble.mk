82bf60d06d17f3dc40efcb434f941abf2d5c1449
June Tate-Gans <jtgans@google.com>
2018-05-25 02:56:53 -0700

    preamble: Add ping, alsa, and standard utilities
    This adds in ping and alsa-utils, as well as a whole smorgasbord of packages
    from the tasksel "standard" package install that is always run as part of the
    debian installer. It increases the size of the rootfs a bit, but also helps out
    with the rough edges considerably more.
    
    Change-Id: Iceab32ad5143732a2ecd6553030a4aa6eb9a8721

--- preamble.mk
+++ preamble.mk
@@ -9,34 +9,76 @@ DEBOOTSTRAP_TARBALL := $(ROOTDIR)/cache/debootstrap.tgz
 DEBOOTSTRAP_TARBALL_SHA256 := $(ROOTDIR)/cache/debootstrap.tgz.sha256sum
 
 DEBOOTSTRAP_EXTRA := \
+	alsa-utils \
+	apt-listchanges \
+	apt-utils \
 	avahi-daemon \
+	bash-completion \
 	bluez \
+	cpio \
+	cron \
 	dbus \
+	debconf-i18n \
 	debian-archive-keyring \
+	debian-faq \
 	dialog \
+	dmidecode \
+	doc-debian \
+	file \
 	firmware-atheros \
+	gettext-base \
+	gnupg \
+	hdparm \
+	ifupdown \
+	init \
+	iptables \
+	iputils-ping \
 	isc-dhcp-client \
+	isc-dhcp-common \
+	krb5-locales \
 	less \
+	libclass-isa-perl \
+	liblockfile-bin \
 	libpam-systemd \
+	libswitch-perl \
 	locales \
+	logrotate \
+	lsof \
 	lxde \
 	man-db \
+	manpages \
+	nano \
+	ncurses-term \
 	net-tools \
+	netbase \
+	netcat-traditional \
 	network-manager \
 	openbox-lxde-session \
 	openssh-server \
 	parted \
+	pciutils \
 	pulseaudio \
+	python \
+	python-minimal \
+	python2.7 \
+	python3-reportbug \
+	reportbug \
+	rsyslog \
 	sudo \
 	systemd \
 	systemd-sysv \
 	tasksel \
+	telnet \
+	traceroute \
 	vim \
+	wamerican \
+	wget \
+	whiptail \
 	wireless-tools \
+	wpasupplicant \
 	xorg \
-	xserver-xorg-video-all \
 	xserver-xorg-input-all \
-	wpasupplicant
+	xserver-xorg-video-all
 
 DEBOOTSTRAP_ARGS := \
 		--arch=arm64 \
