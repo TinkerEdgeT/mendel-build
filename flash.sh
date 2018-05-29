70f5bcc408230394d751a2141a9266485c24378a
Alex Van Damme <atv@google.com>
2018-05-29 10:06:17 -0700

    Add misc partition, identify root by PARTUUID
    - Add back the misc partiton: fastboot stores boot data here for stuff
    like reboot-bootloader
    - Update kernel cmdline and fstab to mount / by GPT PARTUUID: now we
    should be able to move that partition around freely, as needed.
    
    Change-Id: If24c293671dbefda008184684f46e6796357296a

--- flash.sh
+++ flash.sh
@@ -8,6 +8,7 @@ set -e
 fastboot flash bootloader0 ${PRODUCT_OUT}/u-boot.imx
 fastboot flash gpt ${PRODUCT_OUT}/partition-table.img
 fastboot reboot-bootloader
+fastboot erase misc
 fastboot flash boot ${PRODUCT_OUT}/boot.img
 fastboot flash rootfs ${PRODUCT_OUT}/rootfs.img
 fastboot reboot
