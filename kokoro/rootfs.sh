654eea55ebdfd1f172cb0ed3beb874e5dc77a80f
Alex Van Damme <atv@google.com>
2018-05-29 18:21:01 -0700

    Remove -R from umount in Kokoro script
    Change-Id: I389543b125edac5fc5f8723db801c4b77fcc8485

--- kokoro/rootfs.sh
+++ kokoro/rootfs.sh
@@ -10,7 +10,7 @@ popd
 m prereqs
 mm debootstrap make-bootstrap-tarball
 mm rootfs rootfs_raw || true
-sudo umount -R git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs || true
+sudo umount git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs || true
 
 cp git/cache/debootstrap.tgz $KOKORO_ARTIFACTS_DIR
 cp git/cache/debootstrap.tgz.sha256sum $KOKORO_ARTIFACTS_DIR
