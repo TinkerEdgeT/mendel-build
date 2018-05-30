23acda2994801149f1b3a4a234c5ffc96028a8a3
Alex Van Damme <atv@google.com>
2018-05-30 11:06:26 -0700

    Update debootstrap during the Kokoro build
    - Debootstrap on Kokoro looks to have a bug that prevents our build from
    suceeding, with errors about systemd pre-dependencies. Looks like this
    was an actual bug in debootstrap: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=857338
    - Update to the debootstrap from Ubuntu Xenial during our build, as the
    bug should be fixed in that version.
    
    Change-Id: I3c1316a98ddc654a0bf12aa755cbe4a7d11fa24b

--- kokoro/rootfs.sh
+++ kokoro/rootfs.sh
@@ -7,12 +7,16 @@ ln -srf build/Makefile .
 source build/setup.sh
 popd
 
+# Debootstrap on 14.04 is very old and buggy. Update to the 16.04 version.
+echo "deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install debootstrap/xenial
+
 m prereqs
 mm debootstrap make-bootstrap-tarball
-mm rootfs rootfs_raw || true
-sudo umount git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs || true
+mm rootfs rootfs_raw
 
 cp git/cache/debootstrap.tgz $KOKORO_ARTIFACTS_DIR
 cp git/cache/debootstrap.tgz.sha256sum $KOKORO_ARTIFACTS_DIR
 cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img $KOKORO_ARTIFACTS_DIR
-# cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img.sha256sum $KOKORO_ARTIFACTS_DIR
+cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img.sha256sum $KOKORO_ARTIFACTS_DIR
