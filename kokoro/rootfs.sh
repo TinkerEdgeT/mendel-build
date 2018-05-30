be35732f7e8becaae2db1e08265db80814c898e2
Alex Van Damme <atv@google.com>
2018-05-29 17:46:41 -0700

    Skip some errors to help debug Kokoro
    - Eat the return code for making the rootfs, it's failing on Kokoro:
        W: Failure while installing base packages.  This will be re-attempted up
        to five times.
        W: See //debootstrap/debootstrap.log for details (possibly the package
        archive is at fault)
      This'll give an easy way to examine the logs and diagnose the problem.
    
    Change-Id: I55b493033e363f5e87f6b6058200c092333cf7f3

--- kokoro/rootfs.sh
+++ kokoro/rootfs.sh
@@ -9,9 +9,10 @@ popd
 
 m prereqs
 mm debootstrap make-bootstrap-tarball
-mm rootfs rootfs_raw
+mm rootfs rootfs_raw || true
+sudo umount -R git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs || true
 
 cp git/cache/debootstrap.tgz $KOKORO_ARTIFACTS_DIR
 cp git/cache/debootstrap.tgz.sha256sum $KOKORO_ARTIFACTS_DIR
 cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img $KOKORO_ARTIFACTS_DIR
-cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img.sha256sum $KOKORO_ARTIFACTS_DIR
+# cp git/out/target/product/imx8m_phanbell/obj/ROOTFS/rootfs.raw.img.sha256sum $KOKORO_ARTIFACTS_DIR
