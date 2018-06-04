9b07143a674f02df41b95d01835652c027df75ae
Alex Van Damme <atv@google.com>
2018-06-04 16:35:55 -0700

    Symlink Makefile for continuous build
    Change-Id: Ie1871b5a7adc42edf50256592123137d03b1f767

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Symlink the Makefile, like it would be if repo checked this out.
+# Otherwise, sourcing setup.sh doesn't work as expected.
+ln -sfr git/build/Makefile git/Makefile
 source git/continuous-build/build/setup.sh
 TARBALL_FETCH_ROOT_DIRECTORY=${KOKORO_GFILE_DIR} m
 
