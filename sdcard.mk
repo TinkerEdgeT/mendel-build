7ed23d7a52a9032bd9211c4ecbfd2cdd34767bf2
Alex Van Damme <atv@google.com>
2018-06-07 16:39:49 -0700

    Fix sdcard build
    - After some of the refactoring that's happened, we ended up in a state
    where the sparse rootfs img was being copied into the sdcard instead of
    the non-sparse. Whoops.
    - Also, update the boot script a little bit: Depending on the boot
    device, we either use the PARTUUID of the system partition on MMC, or
    the /dev path for the SD card. Unfortunately, the PARTUUID changes each
    time the SD card image is generated, so we can't currently boot that by
    UUID.
    
    Change-Id: I66f132095adda79efaed6d6c0b7d387f33626754

--- sdcard.mk
+++ sdcard.mk
@@ -7,14 +7,17 @@ include $(ROOTDIR)/build/preamble.mk
 sdcard: $(PRODUCT_OUT)/sdcard.img
 sdcard-xz: $(PRODUCT_OUT)/sdcard.img.xz
 
-$(PRODUCT_OUT)/sdcard.img: $(PRODUCT_OUT)/rootfs.img $(PRODUCT_OUT)/boot.img $(PRODUCT_OUT)/u-boot.imx
+$(PRODUCT_OUT)/sdcard.img: $(ROOTDIR)/build/rootfs.mk $(ROOTDIR)/build/boot.mk $(ROOTDIR)/build/u-boot.mk
+	+make -f $(ROOTDIR)/Makefile boot-targets
+	+make -f $(ROOTDIR)/Makefile rootfs
 	fallocate -l 4G $(PRODUCT_OUT)/sdcard.img
 	parted -s $(PRODUCT_OUT)/sdcard.img mklabel msdos
 	parted -s $(PRODUCT_OUT)/sdcard.img unit MiB mkpart primary fat32 8 40
 	parted -s $(PRODUCT_OUT)/sdcard.img unit MiB mkpart primary 40 4095
 	dd if=$(PRODUCT_OUT)/u-boot.imx of=$(PRODUCT_OUT)/sdcard.img conv=notrunc seek=66 bs=512
 	dd if=$(PRODUCT_OUT)/boot.img of=$(PRODUCT_OUT)/sdcard.img conv=notrunc seek=8 bs=1M
-	dd if=$(PRODUCT_OUT)/rootfs.img of=$(PRODUCT_OUT)/sdcard.img conv=notrunc seek=40 bs=1M
+	dd if=$(PRODUCT_OUT)/obj/ROOTFS/rootfs.patched.img \
+		of=$(PRODUCT_OUT)/sdcard.img conv=notrunc seek=40 bs=1M
 
 $(PRODUCT_OUT)/sdcard.img.xz: $(PRODUCT_OUT)/sdcard.img
 	xz -k -T0 -0 $(PRODUCT_OUT)/sdcard.img
