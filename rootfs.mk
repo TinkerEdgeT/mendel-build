67e70846df91efabbc25435fc8e0aac353c9cc59
Alex Van Damme <atv@google.com>
2018-06-07 12:54:59 -0700

    Add CONFIG_SPI_IMX to defconfig, enable spidev
    - CONFIG_SPI_IMX is the driver for SPI master (ecspi). Also turn on the
    spidev module to access from userspace.
    
    Change-Id: Id1dd932c4c583960b71234b86e8a20be35dda7b1

--- rootfs.mk
+++ rootfs.mk
@@ -51,6 +51,7 @@ adjustments:
 	echo "enterprise" | sudo tee $(ROOTFS_DIR)/etc/hostname
 	echo "127.0.0.1 enterprise" | sudo tee -a $(ROOTFS_DIR)/etc/hosts
 	echo "en_US.UTF-8 UTF-8" | sudo tee -a $(ROOTFS_DIR)/etc/locale.gen
+	echo "spidev" | sudo tee -a $(ROOTFS_DIR)/etc/modules
 	sudo chroot $(ROOTFS_DIR) locale-gen
 	sudo chroot $(ROOTFS_DIR) mkdir -p /home/enterprise
 	sudo chroot $(ROOTFS_DIR) adduser enterprise --home /home/enterprise --shell /bin/bash --disabled-password --gecos ""
