5baf71567c3f916cbfd2c8f849527ba2d676d63d
Alex Van Damme <atv@google.com>
2018-06-07 17:01:57 -0700

    Add sdcard to Kokoro continuous build
    Change-Id: If7f3b13467d32e65f5393c49c5ed1a6b3de5b292

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -17,6 +17,7 @@ export DEBOOTSTRAP_TARBALL_REVISION=.
 export ROOTFS_REVISION=.
 
 m
+m sdcard
 
 pushd ${ROOTDIR}
 python3 ${ROOTDIR}/build/create_release_manifest.py \
@@ -28,3 +29,4 @@ cp ${PRODUCT_OUT}/u-boot.imx ${KOKORO_ARTIFACTS_DIR}
 cp ${PRODUCT_OUT}/boot.img ${KOKORO_ARTIFACTS_DIR}
 cp ${PRODUCT_OUT}/partition-table.img ${KOKORO_ARTIFACTS_DIR}
 cp ${PRODUCT_OUT}/rootfs.img ${KOKORO_ARTIFACTS_DIR}
+cp ${PRODUCT_OUT}/sdcard.img ${KOKORO_ARTIFACTS_DIR}
