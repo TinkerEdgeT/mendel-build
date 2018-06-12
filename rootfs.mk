e791bfca3a75b37093633ab2db13218e536ae6ec
June Tate-Gans <jtgans@google.com>
2018-06-12 13:57:57 -0700

    enterprise: Remove the old codename
    We'll be calling this the "AIY Board" publicly when we launch.
    
    Change-Id: Ie6fb153a3984b426f79587ee55ce536241d8b8b5

--- rootfs.mk
+++ rootfs.mk
@@ -43,25 +43,25 @@ firmware:
 adjustments:
 	sudo rm -f $(ROOTFS_DIR)/etc/ssh/ssh_host_*
 	sudo rm -f $(ROOTFS_DIR)/var/log/bootstrap.log
-	echo "enterprise" | sudo tee $(ROOTFS_DIR)/etc/hostname
-	echo "127.0.0.1 enterprise" | sudo tee -a $(ROOTFS_DIR)/etc/hosts
+	echo "aiy" | sudo tee $(ROOTFS_DIR)/etc/hostname
+	echo "127.0.0.1 aiy" | sudo tee -a $(ROOTFS_DIR)/etc/hosts
 	echo "en_US.UTF-8 UTF-8" | sudo tee -a $(ROOTFS_DIR)/etc/locale.gen
 	echo "spidev" | sudo tee -a $(ROOTFS_DIR)/etc/modules
 	sudo chroot $(ROOTFS_DIR) locale-gen
-	sudo chroot $(ROOTFS_DIR) mkdir -p /home/enterprise
-	sudo chroot $(ROOTFS_DIR) adduser enterprise --home /home/enterprise --shell /bin/bash --disabled-password --gecos ""
+	sudo chroot $(ROOTFS_DIR) mkdir -p /home/aiy
+	sudo chroot $(ROOTFS_DIR) adduser aiy --home /home/aiy --shell /bin/bash --disabled-password --gecos ""
 	for group in $(USER_GROUPS); do \
-		sudo chroot $(ROOTFS_DIR) adduser enterprise $$group; \
+		sudo chroot $(ROOTFS_DIR) adduser aiy $$group; \
 	done
-	sudo chroot $(ROOTFS_DIR) chown enterprise:enterprise /home/enterprise
-	sudo chroot $(ROOTFS_DIR) bash -c "echo 'enterprise:enterprise' | chpasswd"
+	sudo chroot $(ROOTFS_DIR) chown aiy:aiy /home/aiy
+	sudo chroot $(ROOTFS_DIR) bash -c "echo 'aiy:aiy' | chpasswd"
 	echo "nameserver 8.8.8.8" | sudo tee $(ROOTFS_DIR)/etc/resolv.conf
 
 	sudo chroot $(ROOTFS_DIR) systemctl enable ssh
 	sudo chroot $(ROOTFS_DIR) systemctl enable bluetooth
 	sudo chroot $(ROOTFS_DIR) systemctl enable avahi-daemon
 	sudo chroot $(ROOTFS_DIR) systemctl enable NetworkManager
-	echo "enterprise	ALL=(ALL) ALL" |sudo tee -a $(ROOTFS_DIR)/etc/sudoers
+	echo "aiy	ALL=(ALL) ALL" |sudo tee -a $(ROOTFS_DIR)/etc/sudoers
 
 	sudo $(ROOTDIR)/build/fix_permissions.sh -p $(ROOTDIR)/build/permissions.txt -t $(ROOTFS_DIR)
 
