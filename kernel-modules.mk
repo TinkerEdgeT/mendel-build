a147e35e7e493ccaa902bb161fb6d802ef4d9b83
Alex Van Damme <atv@google.com>
2018-07-09 17:55:56 -0700

    Allow installing kernel module from prebuilt deb
    - Add a concept of REQUIRED_MODULES to the kernel-modules build. If a
    module is in REQUIRED_MODULES, but the source is not available under
    $ROOTDIR/modules, try to fetch it from $PREBUILT_MODULES_ROOT.
    
    Change-Id: Ie240402e83b300c0d24412d1281f9a8ffc118919

--- kernel-modules.mk
+++ kernel-modules.mk
@@ -4,11 +4,21 @@ endif
 
 include $(ROOTDIR)/build/preamble.mk
 
+REQUIRED_MODULES := apex
+
 MODULES_DIRS := $(wildcard $(ROOTDIR)/modules/*)
 MODULES := $(foreach module,$(MODULES_DIRS),$(notdir $(module)))
+MODULES := $(sort $(MODULES) $(REQUIRED_MODULES))
+
+PREBUILT_MODULES_ROOT ?= /google/data/ro/teams/spacepark/enterprise/debs
 
 define make-module-target
 $1: $(PRODUCT_OUT)/.$1
+ifeq (,$$(wildcard $(ROOTDIR)/modules/$1/*))
+$(PRODUCT_OUT)/.$1: $(PRODUCT_OUT)
+	$$(warning "No source for $1")
+	cp $(PREBUILT_MODULES_ROOT)/$1.deb $(PRODUCT_OUT)
+else
 $(PRODUCT_OUT)/.$1: $(shell find $(ROOTDIR)/modules/$1 -type f) $(KERNEL_OUT_DIR)/.config $(KERNEL_OUT_DIR)/arch/arm64/boot/Image
 	mkdir -p $(PRODUCT_OUT)/obj/MODULE_OBJ/$1/debian
 	cp -afs $(ROOTDIR)/modules/$1/* $(PRODUCT_OUT)/obj/MODULE_OBJ/$1
@@ -21,6 +31,7 @@ $(PRODUCT_OUT)/.$1: $(shell find $(ROOTDIR)/modules/$1 -type f) $(KERNEL_OUT_DIR
 		KOUT=$(KERNEL_OUT_DIR) \
 		debian/rules kdist_image
 	touch $(PRODUCT_OUT)/.$1
+endif
 endef
 
 $(foreach module,$(MODULES),$(eval $(call make-module-target,$(module))))
