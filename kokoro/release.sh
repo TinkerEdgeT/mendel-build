078ac5a56c9c96de263d419011ccf7b3046fc3c7
Alex Van Damme <atv@google.com>
2018-07-17 18:12:59 -0700

    Setup environment in release.sh
    - These don't propagate upwards from invoking continuous.sh, so be sure
    to define them.
    
    Change-Id: I86a9ee7a44bbf2b497135d2bedeaafd34473da2b

--- kokoro/release.sh
+++ kokoro/release.sh
@@ -7,6 +7,16 @@ set -x
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 bash ${SCRIPT_DIR}/continuous.sh
 
+pushd git/continuous-build
+source build/setup.sh
+popd
+
+export TARBALL_FETCH_ROOT_DIRECTORY=${KOKORO_GFILE_DIR}
+export PREBUILT_MODULES_ROOT=${KOKORO_GFILE_DIR}
+export PREBUILT_DOCKER_ROOT=${KOKORO_GFILE_DIR}
+export DEBOOTSTRAP_TARBALL_REVISION=.
+export ROOTFS_REVISION=.
+
 m docker-make-repo
 
 m sign-repo
