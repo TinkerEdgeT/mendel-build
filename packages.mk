32634661b98b93c5c87c3d2372744f0e4624a3e0
Alex Van Damme <atv@google.com>
2018-08-23 12:15:02 -0700

    Convert ALLARCH_ and ARM64_ packages to pbuilder
    - Move all the packages from ALLARCH and ARM64 sets to pbuilder calls
    - Remove unused macros from old-style package building
    
    Change-Id: I96c2de5753c8092ba1e7050f742667f7c440dee4

--- packages.mk
+++ packages.mk
@@ -4,43 +4,11 @@ endif
 
 include $(ROOTDIR)/build/preamble.mk
 
-# Packages which will run on any architecture.
-ALLARCH_PACKAGE_NAMES := \
-		 aiy-board-audio \
-		 aiy-board-gadget \
-		 aiy-board-keyring \
-		 aiy-board-tools \
-		 aiy-board-wlan
-
-# Packages which require ARM64 binaries to be built.
-ARM64_PACKAGE_NAMES := \
-		imx-gpu-viv
-
 EQUIVS_PACKAGE_NAMES := $(notdir $(shell find $(ROOTDIR)/packages/equivs -maxdepth 1 -type f))
 
-ALL_PACKAGE_NAMES := $(ALLARCH_PACKAGE_NAMES) $(ARM64_PACKAGE_NAMES) $(EQUIVS_PACKAGE_NAMES)
+ALL_PACKAGE_NAMES := $(EQUIVS_PACKAGE_NAMES)
 
 BUILDPACKAGE_CMD := dpkg-buildpackage -b -rfakeroot -us -uc -tc
-
-define make-package-target
-	find $(ROOTDIR)/packages -maxdepth 1 -type f -name '$1*' -exec mv -f {} $(PRODUCT_OUT) \;
-	touch $(PRODUCT_OUT)/.$1
-endef
-
-define make-allarch-package-target
-$(PRODUCT_OUT)/.$1: $$(shell find $(ROOTDIR)/packages/$1 -type f)
-	cd $(ROOTDIR)/packages/$1; $(BUILDPACKAGE_CMD)
-$(call make-package-target,$1)
-endef
-
-define make-arm64-package-target
-$(PRODUCT_OUT)/.$1: $$(shell find $(ROOTDIR)/packages/$1 -type f) $(ROOTDIR)/cache/arm64-builder.tar
-	docker load -i $(ROOTDIR)/cache/arm64-builder.tar
-	docker run --rm --tty \
-	  -v $(ROOTDIR)/packages:/packages arm64-builder \
-	  /bin/bash -c 'cd /packages/$1; $(BUILDPACKAGE_CMD);'
-$(call make-package-target,$1)
-endef
 
 define make-equivs-package-target
 $(PRODUCT_OUT)/.$1: $(ROOTDIR)/packages/equivs/$1
@@ -102,12 +70,6 @@ $(PRODUCT_OUT)/.$1-pbuilder: \
 .PHONY:: $1
 endef
 
-# Generate ARM64 targets
-$(foreach package,$(ARM64_PACKAGE_NAMES),$(eval $(call make-arm64-package-target,$(package))))
-
-# Generate ALL arch targets
-$(foreach package,$(ALLARCH_PACKAGE_NAMES),$(eval $(call make-allarch-package-target,$(package))))
-
 # Generate EQUIVS targets
 $(foreach package,$(EQUIVS_PACKAGE_NAMES),$(eval $(call make-equivs-package-target,$(package))))
 
@@ -118,6 +80,14 @@ $(eval $(call make-pbuilder-package-target,uboot-imx,uboot-imx,imx-atf imx-firmw
 
 $(eval $(call make-pbuilder-package-target,wayland-protocols-imx,wayland-protocols-imx))
 $(eval $(call make-pbuilder-package-target,weston-imx,weston-imx,wayland-protocols-imx))
+
+$(eval $(call make-pbuilder-package-target,imx-gpu-viv,packages/imx-gpu-viv))
+
+$(eval $(call make-pbuilder-package-target,aiy-board-audio,packages/aiy-board-audio))
+$(eval $(call make-pbuilder-package-target,aiy-board-gadget,packages/aiy-board-gadget))
+$(eval $(call make-pbuilder-package-target,aiy-board-keyring,packages/aiy-board-keyring))
+$(eval $(call make-pbuilder-package-target,aiy-board-tools,packages/aiy-board-tools))
+$(eval $(call make-pbuilder-package-target,aiy-board-wlan,packages/aiy-board-wlan))
 
 packages:: $(foreach package,$(ALL_PACKAGE_NAMES),$(PRODUCT_OUT)/.$(package)) $(PBUILDER_TARGETS)
 
