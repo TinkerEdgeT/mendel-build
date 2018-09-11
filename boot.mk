c6affa8c19cb4643932959f52f33e6f3857744d6
Alex Van Damme <atv@google.com>
2018-09-10 17:12:26 -0700

    Make boot.img target just generate filesystem
    - All the files in /boot are under package control now,
    so just build a filesystem. It'll get populated with stuff when we
    install packages.
    
    Change-Id: Id604560ae5106f0da6c8850b3ec35072270caf70

--- boot.mk
+++ boot.mk
@@ -6,26 +6,14 @@ include $(ROOTDIR)/build/preamble.mk
 
 boot: $(PRODUCT_OUT)/boot.img
 
-$(PRODUCT_OUT)/boot.img: $(PRODUCT_OUT)/u-boot.imx \
-                         $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/dts/freescale/fsl-imx8mq-phanbell.dtb \
-                         $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/Image \
-                         $(PRODUCT_OUT)/obj/BOOT_OBJ/boot.scr
-	mkdir -p $(PRODUCT_OUT)/boot
-	cp $(PRODUCT_OUT)/obj/BOOT_OBJ/boot.scr \
-	   $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/dts/freescale/fsl-imx8mq-phanbell.dtb \
-	   $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/Image \
-		 $(PRODUCT_OUT)/boot/
-	genext2fs -d $(PRODUCT_OUT)/boot/ -B 4096 -b 32768 $(PRODUCT_OUT)/boot.img
-
-$(PRODUCT_OUT)/obj/BOOT_OBJ/boot.scr: $(HOST_OUT)/bin/mkimage
-	mkdir -p $(PRODUCT_OUT)/obj/BOOT_OBJ
-	$(HOST_OUT)/bin/mkimage -A arm -T script -O linux -d $(ROOTDIR)/board/boot.txt $(PRODUCT_OUT)/obj/BOOT_OBJ/boot.scr
+$(PRODUCT_OUT)/boot.img:
+	fallocate -l 128M $@
+	mkfs.ext2 -F $@
 
 targets::
 	@echo "boot - builds the kernel and boot partition"
 
 clean::
 	rm -f $(PRODUCT_OUT)/boot.img
-	rm -rf $(PRODUCT_OUT)/obj/BOOT_OBJ
 
 .PHONY:: boot
