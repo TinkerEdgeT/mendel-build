ea8bfe0b9e0815ccbae45b415846336af3d1918a
Leslie Yu <Leslie_Yu@asus.com>
2020-06-27 17:38:37 +0800

    Add build script for Tinker Edge T
    Change-Id: I3054a329d05bdf45259f62ecef7ddcff72db9e60

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $VERSION_NUMBER ]; then
+	VERSION_NUMBER="$VERSION_NUMBER"-"$(date  +%Y%m%d)"
+	RELEASE_NAME=Tinker_Edge_T-Mendel-Chef-V"$VERSION_NUMBER"
+fi
+echo "VERSION_NUMBER: $VERSION_NUMBER"
+
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
+TOP_DIR=$(realpath $COMMON_DIR/..)
+
+source build/setup.sh
+m
+m docker-sdcard
+m docker-recovery
+
+if [ "$VERSION_NUMBER" ]; then
+	IMAGE_PATH=$TOP_DIR/out/target/product/imx8m_phanbell
+	STUB_PATH=IMAGE/"$RELEASE_NAME"
+	export STUB_PATH=$TOP_DIR/$STUB_PATH
+	mkdir -p $STUB_PATH/$RELEASE_NAME
+	cp -rp $TOP_DIR/board/flash/. $STUB_PATH/$RELEASE_NAME
+	cp $TOP_DIR/board/flash_uboot_and_force_fastboot_mode.cmd $STUB_PATH/.
+        cd $IMAGE_PATH
+	cp sdcard_arm64.img $STUB_PATH
+	cp boot_arm64.img partition-table-8gb.img recovery.img rootfs_arm64.img u-boot.imx $STUB_PATH/$RELEASE_NAME
+	cd -
+	cd $STUB_PATH
+	zip -r $RELEASE_NAME.zip $RELEASE_NAME
+	sha256sum $RELEASE_NAME.zip > $RELEASE_NAME.zip.sha256sum
+	rm -rf $RELEASE_NAME
+	cd -
+fi
